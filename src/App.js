3066e427f0a9d85f90305a340034a0cc7729c2c2
Mohamed Hasir <mdhasir96@yahoo.com>
2023-10-23 23:04:20 +0530

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hasir Skyboy</code> and save to reload.
+          <code>Hasir Skyboy</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Hasir
         </a>
       </header>
     </div>
